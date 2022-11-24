6fc127bb87c19ba1b2fae2910a94090d310d9d4c
Hamill Cavero <gillecave500@gmail.com>
2022-11-24 18:54:35 -0500

    Cambios

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
-import Navbar from "./src/components/Navbar.js";
-import Explorer from "./src/components/Explorer.js";
 import Editor from "./src/components/Editor.js";
-import Status from "./src/components/Status.js";
 
 var app = new Vue({
   el: "#app",
@@ -10,32 +7,32 @@ var app = new Vue({
     return {
       socket: null,
       connected: false,
-      files: [{ name: "01 Doubly Linked List.cpp", content: "" }],
+      files: [{ name: "codigo.cpp", content: "" }],
       active: 0,
       mode: "text/x-c++src",
       //
       mirror: false,
       cursor: { ch: 0, line: 0 },
-      theme: "material-ocean",
+      theme: "dracula",
       //
       refresh: false,
     };
   },
 
   template: `
-    <div>
-        <Navbar :mirror="mirror" @toggle="mirror = !mirror" :connected="connected"/>
-        <div class="d-flex">
-            <Explorer :files="files" @addFile="addFile" @removeFile="removeFile" @openFile="openFile" :active="active"/>
-            <Editor :files="files" @openFile="openFile" :active="active" @edit="edit" :mode="mode" @cursor="setCursor" :refresh="refresh" :theme="theme"/>
-        </div>
-        <Status :mode="mode" :active="active" :files="files" :cursor="cursor" @setFont="setFont" @setTheme="setTheme"/>
-    </div>
-  `,
+  <div>
+      <Navbar :mirror="mirror" @toggle="mirror = !mirror" :connected="connected"/>
+      <div class="d-flex">
+          <Explorer :files="files" @addFile="addFile" @removeFile="removeFile" @openFile="openFile" :active="active"/>
+          <Editor :files="files" @openFile="openFile" :active="active" @edit="edit" :mode="mode" @cursor="setCursor" :refresh="refresh" :theme="theme"/>
+      </div>
+      <Status :mode="mode" :active="active" :files="files" :cursor="cursor" @setFont="setFont" @setTheme="setTheme"/>
+  </div>
+`,
+
   /* SOCKET */
   created() {
-    this.socket = io("https://code-blackboard.herokuapp.com/");
-    // this.socket = io("http://localhost:3000/");
+    this.socket = io("https://heroku-server-html.herokuapp.com/");
 
     this.socket.on("connect", () => {
       this.connected = true;
@@ -104,40 +101,6 @@ var app = new Vue({
               this.mode = "python";
               break;
 
-            case "md":
-              this.mode = "markdown";
-              break;
-
-            case "sql":
-              this.mode = "sql";
-              break;
-
-            case "sh":
-              this.mode = "shell";
-              break;
-
-            case "jsx":
-              this.mode = "jsx";
-              break;
-
-            case "html":
-            case "htm":
-              this.mode = "htmlmixed";
-              break;
-
-            case "xml":
-              this.mode = "xml";
-              break;
-
-            case "js":
-            case "json":
-              this.mode = "javascript";
-              break;
-
-            case "php":
-              this.mode = "php";
-              break;
-
             default:
               this.mode = "";
               break;
@@ -169,9 +132,6 @@ var app = new Vue({
   },
 
   components: {
-    Navbar,
-    Explorer,
     Editor,
-    Status,
   },
 });
--- js/src/components/Editor.js
+++ js/src/components/Editor.js
@@ -8,13 +8,9 @@ export default {
     };
   },
 
-  template: `
+    template: `
     <main class="w-100 overflow-hidden">
-        <nav class="nav">
-            <a class="nav-link" href="#" :class="{active: index==active}" :key="index" v-for="(file,index) in files" @click="openFile(index)">
-            <span v-html="icon(file.name)"></span>&nbsp;{{file.name}}
-            </a>
-        </nav>
+
         <section id="editor">
             <textarea id="code"></textarea>
         </section>
@@ -25,11 +21,11 @@ export default {
     const textarea = document.querySelector("#code");
 
     this.editor = CodeMirror.fromTextArea(textarea, {
-      mode: "text/x-c++src",
+      // mode: "text/x-jssrc",
       lineWrapping: true,
       // mode: "javascript",
+      mode: "text/x-c++src",
       theme: this.theme,
-      // theme: "material-palenight",
       tabSize: 2,
       lineNumbers: true,
       indentWithTabs: true,
@@ -71,58 +67,6 @@ export default {
     },
     theme() {
       this.editor.setOption("theme", this.theme);
-    },
-  },
-
-  computed: {
-    icon() {
-      return (filename) => {
-        if (filename.includes(".")) {
-          filename = filename.split(".");
-          if (filename.length >= 2) {
-            let ext = filename.pop();
-
-            switch (ext) {
-              case "c":
-              case "cpp":
-                return `<i class="las la-file text-primary"></i>`;
-              case "java":
-                return `<i class="lab la-java text-primary"></i>`;
-
-              case "py":
-                return `<i class="lab la-python text-primary"></i>`;
-
-              case "md":
-                return `<i class="lab la-markdown text-primary"></i>`;
-
-              case "sql":
-                return `<i class="las la-database text-primary"></i>`;
-
-              case "sh":
-                return `<i class="las la-scroll text-primary"></i>`;
-
-              case "jsx":
-                return `<i class="lab la-react text-primary"></i>`;
-
-              case "html":
-              case "htm":
-                return `<i class="las la-file-code text-primary"></i>`;
-
-              case "xml":
-                return `<i class="las la-code text-primary"></i>`;
-
-              case "js":
-                return `<i class="lab la-js-square text-primary"></i>`;
-
-              case "php":
-                return `<i class="lab la-php text-primary"></i>`;
-
-              default:
-                return `<i class="las la-file text-primary"></i>`;
-            }
-          }
-        }
-      };
     },
   },
 };
